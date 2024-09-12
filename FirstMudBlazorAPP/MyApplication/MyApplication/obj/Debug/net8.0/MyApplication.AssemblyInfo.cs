0555f046748012aa8d884dc5e27079becbbb1e4e
kojhack <122357318+kojhack@users.noreply.github.com>
2024-09-12 15:47:18 +0000

    modified

--- FirstMudBlazorAPP/MyApplication/MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ FirstMudBlazorAPP/MyApplication/MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e42230c04870cd94e35ae5980c44036ae192a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58bf5e0f121177c00b3d5a1a78d1926cbb07ba95")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
