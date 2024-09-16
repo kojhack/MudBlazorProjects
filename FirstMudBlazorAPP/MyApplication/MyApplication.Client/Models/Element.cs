709a486b5b7b5f634e289545b3fb4ae8b8693021
kojhack <122357318+kojhack@users.noreply.github.com>
2024-09-16 18:24:11 +0000

    modified

---
+++ FirstMudBlazorAPP/MyApplication/MyApplication.Client/Models/Element.cs
@@ -0,0 +1,9 @@
+public class Element
+{
+    public int Number { get; set; }
+    public string Sign { get; set; }="";
+    public string Name { get; set; }="";
+    public string Position { get; set; }="";
+    public double Molar { get; set; }
+    public string Group { get; set; }="";
+}
